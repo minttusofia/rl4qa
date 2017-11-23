077d8b85728d945501aa446bd9658b6a93035861
Minttu Alakuijala <minttu.alakuijala@gmail.com>
2017-11-23 13:18:48 +0000

    Jump to next doc in ranking if read already; show fastQA's answer

--- playground/datareader.py
+++ playground/datareader.py
@@ -4,8 +4,8 @@ from random import randint
 from ir.search_engine import SearchEngine
 from playground.nouns import nounPhrases
 
-#from jack import readers
-#from jack.core import QASetting
+from jack import readers
+from jack.core import QASetting
 
 class Question():
     def __init__(self, json):
@@ -15,11 +15,21 @@ class Question():
         self.candidates = json['candidates']
         self.supports = json['supports']
 
+
 def showQuestion(question):
     print(question.query)
 
+
 def showNouns(doc):
     print(nounPhrases(doc))
+
+
+def showRCAnswer(reader, query, doc):
+    print('Reading Comprehension answers:')
+    answers = reader([QASetting(question=query, support=[doc])])
+    for a in answers:
+        print(a.text, '\tscore:', a.score)
+
 
 def playground(automatic_first_query=False):
     index_filename = "se_index"
@@ -33,6 +43,7 @@ def playground(automatic_first_query=False):
         dataset = dataset[:subset_size]
         index_filename += '_' + str(subset_size)
     se = SearchEngine(dataset, load_from_path=index_filename)
+    reader = readers.reader_from_file("./fastqa_reader")
 
     while True:
         q_i = randint(0, len(dataset))
@@ -47,22 +58,30 @@ def playground(automatic_first_query=False):
         while True:
             action = input("Type 'q' for next query, 'a' to submit an answer: ")
             if action.lower() == 'a':
-                submission = input("Submit your answer:")
-                if submission == question.answer:
+                submission = input("Submit your answer: ")
+                if submission.lower() == question.answer.lower():
                     print("That's correct!")
                 else:
-                    print("False, the correct answer is ", question.answer)
+                    show_true = input("Incorrect. Would you like to see the correct answer? (Y/n) ")
+                    if show_true.lower() != 'n':
+                        print("The correct answer is", question.answer)
                 break
             elif action.lower() == 'q':
                 query = input("Query: ")
                 top_idx = se.rank_docs(question.id, query, topk=len(question.supports))#[0]
-                for doc in range(len(top_idx)-1, -1, -1):
+                print('Document ranking:', top_idx)
+                for d in range(len(top_idx)-1, -1, -1):
+                    doc = top_idx[d]
                     if read_this_episode[doc]:
-                       print('[READ]', question.supports[doc])
+                       print(len(top_idx) - d, ': [READ] ', question.supports[doc])
                     else:
                         top_idx = doc
-                print('\n', question.supports[top_idx], '\n')
+                        break
+                print('\n', len(question.supports) - d, ':', question.supports[top_idx], '\n')
+                read_this_episode[top_idx] = True
                 showNouns(question.supports[top_idx])
+                showRCAnswer(reader, query, question.supports[top_idx])
+
         cont = input("Continue? (Y/n) ")
         if cont.lower() == 'n':
             break
