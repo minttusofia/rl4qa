d2de7e77d71be06257df8162f58215f8065e5407
Minttu Alakuijala <minttu.alakuijala@gmail.com>
2018-02-28 14:38:01 +0000

    Add trimming alternative for case where most keys are kept

--- shared/utils.py
+++ shared/utils.py
@@ -1,26 +1,38 @@
 import gc
 import random
+import time
 
 
-def trim_index(dataset, nouns, search_engine):
+def trim_index(dataset, nouns, search_engine, keep_most=False):
     """Trim index and pre-extracted nouns to only include instances included in dataset."""
-    one_type_nouns = {}
-    one_type_shape_for_q = {}
-    one_type_sparse_for_q = {}
-    one_type_vec_for_q = {}
     print('Trimming index...')
-    for q in dataset:
-        one_type_nouns[q['id']] = nouns[q['id']]
-        one_type_shape_for_q[q['id']] = search_engine.shape_for_q[q['id']]
-        one_type_sparse_for_q[q['id']] = search_engine.sparse_for_q[q['id']]
-        one_type_vec_for_q[q['id']] = search_engine.vec_for_q[q['id']]
+    prev_time = time.time()
+    # If most keys are kept, it's faster to delete unused than to copy shared
+    if keep_most:
+        dataset_keys = set([q['id'] for q in dataset])
+        unused_index_keys = set(search_engine.shape_for_q.keys()) - dataset_keys
+        for k in unused_index_keys:
+            del search_engine.shape_for_q[k]
+            del search_engine.sparse_for_q[k]
+            del search_engine.vec_for_q[k]
+            del nouns[k]
+    else:
+        one_type_nouns = {}
+        one_type_shape_for_q = {}
+        one_type_sparse_for_q = {}
+        one_type_vec_for_q = {}
+        for q in dataset:
+            one_type_nouns[q['id']] = nouns[q['id']]
+            one_type_shape_for_q[q['id']] = search_engine.shape_for_q[q['id']]
+            one_type_sparse_for_q[q['id']] = search_engine.sparse_for_q[q['id']]
+            one_type_vec_for_q[q['id']] = search_engine.vec_for_q[q['id']]
 
-    nouns = one_type_nouns
-    search_engine.shape_for_q = one_type_shape_for_q
-    search_engine.sparse_for_q = one_type_sparse_for_q
-    search_engine.vec_for_q = one_type_vec_for_q
+        nouns = one_type_nouns
+        search_engine.shape_for_q = one_type_shape_for_q
+        search_engine.sparse_for_q = one_type_sparse_for_q
+        search_engine.vec_for_q = one_type_vec_for_q
     gc.collect()
-    print('Finished trimming')
+    print('Finished trimming ({} s)'.format(time.time() - prev_time))
     return nouns, search_engine
 
 
